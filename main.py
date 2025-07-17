76e213ee9e720649b7a434813cf3e97325581da4
nannilorenzo93 <nanni.lorenzo93@gmail.com>
2025-07-17 14:13:30 +0200

    added stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -9,8 +11,5 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 main()
